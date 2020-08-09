d237626bf566f9b6a201835e8236c98e4aef147c
Youngjun Yoo <yoo156@purdue.edu>
2020-08-10 00:01:52 +0900

    Update substitution.c

--- substitution.c
+++ substitution.c
@@ -19,7 +19,7 @@ string key;
  
 int main(int argc, string argv[])
 {
-    //Checks if no key has been provided.
+    // Checks if no key has been provided.
     if (argv[1] == NULL)
     {
         printf("You must provide a 26 digit alphabetic key containing each letter once and only once.\n");
@@ -27,11 +27,11 @@ int main(int argc, string argv[])
         return 1;
     }
  
-    //Assigns the key from command line to the global variable 'key' and converts it all to uppercase.
+    // Assigns the key from command line to the global variable 'key' and converts it all to uppercase.
     key = argv[1];
     convertToUpperCase(key);
  
-    //Checks if function validateKey returns false. See line for function.
+    // Checks if function validateKey returns false. See line for function.
     if (!validateKey())
     {
         return 1;
@@ -39,11 +39,11 @@ int main(int argc, string argv[])
  
     string plaintext = get_string("plaintext: ");
     printf("ciphertext: %s\n", convertToCipherText(plaintext)); //See line 119 for function.
-    //Returns 0 to exit program successfully.
+    // Returns 0 to exit program successfully.
     return 0;
 }
  
-//Function to simply loop over every character in the key and convert them to uppercase.
+// Function to simply loop over every character in the key and convert them to uppercase.
 void convertToUpperCase()
 {
     for (int i = 0; i < strlen(key); i++)
@@ -52,28 +52,28 @@ void convertToUpperCase()
     }
 }
  
-//Performs a couple of checks by function calls to verify key is valid as per the specification given.
+// Performs a couple of checks by function calls to verify key is valid as per the specification given.
 bool validateKey()
 {
     bool result = false;
-    if (!verifyKeyOnlyContainsLetters()) //See line 75.
+    if (!verifyKeyOnlyContainsLetters()) // See line 75.
     {
         printf("Key must only contain letters.\n");
         return result;
     }
-    if (strlen(key) != 26) //Makes sure the key is exactly 26 characters long.
+    if (strlen(key) != 26) // Makes sure the key is exactly 26 characters long.
     {
         printf("Key must contain exactly 26 characters.\n");
         return result;
     }
  
-    if (!checkIfKeyContainsAllUniqueCharacters()) //See line 91.
+    if (!checkIfKeyContainsAllUniqueCharacters()) // See line 91.
     {
         printf("Key must contain each character once and only once.\n");
         return result;
     }
  
-    //If all the tests pass, result is assigned true and returned.
+    // If all the tests pass, result is assigned true and returned.
     result = true;
     return result;
 }
@@ -82,8 +82,8 @@ bool verifyKeyOnlyContainsLetters()
 {
     bool result = true;
  
-    //Loops through every character of the key and checks that they're all between the range of the uppercase alphabet.
-    //(key was converted to uppercase earlier to avoid checking the lowercase letters.)
+    // Loops through every character of the key and checks that they're all between the range of the uppercase alphabet.
+    // (key was converted to uppercase earlier to avoid checking the lowercase letters.)
     for (int i = 0; i < strlen(key); i++)
     {
         if (!(key[i] >= 'A' && key[i] <= 'Z'))
@@ -99,22 +99,22 @@ bool checkIfKeyContainsAllUniqueCharacters()
     bool result = false;
     int containsLetter = 0;
  
-    //Function loops through the alphabet checking each character of the key as it goes.
+    // Function loops through the alphabet checking each character of the key as it goes.
     for (char currentChar = 'A'; currentChar <= 'Z'; currentChar++)
     {
         for (int i = 0; i <= strlen(key); i++)
         {
-            //If the key contains the current letter, it incriments the counter variable (containsLetter) and then moves on to the next letter.
+            // If the key contains the current letter, it incriments the counter variable (containsLetter) and then moves on to the next letter.
             if (currentChar == key[i])
             {
                 containsLetter++;
-                //Break statement is essential as once it see's a character in the key it stops and moves on to the next alphabetic character, this checks for duplicate letters within the key.
+                // Break statement is essential as once it see's a character in the key it stops and moves on to the next alphabetic character, this checks for duplicate letters within the key.
                 break;
             }
         }
     }
  
-    //If the counter was successfully incremented to 26 i.e it contains each letter once and only once, result is then assigned true and returned.
+    // If the counter was successfully incremented to 26 i.e it contains each letter once and only once, result is then assigned true and returned.
     if (containsLetter == 26)
     {
         result = true;
@@ -128,17 +128,17 @@ string convertToCipherText(string plaintext)
  
     for (int i = 0; i < strlen(ciphertext); i++)
     {
-        //If block to ignore punctuation.
+        // If block to ignore punctuation.
         if (plaintext[i] == ' ' || plaintext[i] == ',' || plaintext[i] == '.' || plaintext[i] == '?')
         {
-            //DO NOTHING
+            // DO NOTHING
         }
-        //Checks if the plaitext is a capital letter, if so it takes off 97 (see asciichart.com) to get the index to 0 and assigns the corresponding key value to the ciphertext.
+        // Checks if the plaitext is a capital letter, if so it takes off 97 (see asciichart.com) to get the index to 0 and assigns the corresponding key value to the ciphertext.
         if (plaintext[i] >= 65 && plaintext[i] <= 90)
         {
             ciphertext[i] = key[plaintext[i] - 65];
         }
-        //Same as previous step but for lowercase letters. Takes off 97 this time and then converts the assigned character to lowercase as the key is all uppercase characters.
+        // Same as previous step but for lowercase letters. Takes off 97 this time and then converts the assigned character to lowercase as the key is all uppercase characters.
         if (plaintext[i] >= 97 && plaintext[i] <= 122)
         {
             ciphertext[i] = key[plaintext[i] - 97];
